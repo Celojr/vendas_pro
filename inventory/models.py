fdd89e85e447e616750a88073d81edbe8f87007e
Marcelo de Campos Cruz Júnior <marcelo.camposjr16@gmail.com>
2025-04-02 18:46:41 -0300

    feat: product migration

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+  name = models.CharField(max_length=200)
+  price = models.DecimalField(max_digits=5,decimal_places=2)
+  quantity = models.PositiveIntegerField()
+
+  def __str__(self):
+    return self.name
+
+  def available(self):
+    return self.quantity > 0
+
+  def unavailable(self):
+    return not self.available
