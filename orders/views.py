f014e4bef6c6f696876e3ec32c538e07fc877e69
Marcelo de Campos Cruz Júnior <marcelo.camposjr16@gmail.com>
2025-04-09 20:01:53 -0300

    feat: add order index template

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Order
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the orders index.")
+  order_list = Order.objects.all()
+  context = {"order_list": order_list}
+  return render(request, "orders/index.html", context)
+
+def detail(request, order_id):
+  order = Order.objects.get(id=order_id)
+  context = {"order": order}
+  return render(request, "orders/detail.html", context)
