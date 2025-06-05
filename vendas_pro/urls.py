bb5664057ebd33d8625c6da6a5661e8b480a4b45
Marcelo de Campos Cruz Júnior <marcelo.camposjr16@gmail.com>
2025-06-04 22:59:02 -0300

    feat: Updating pages and implementing the home page

--- vendas_pro/urls.py
+++ vendas_pro/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/', include('accounts.urls')),
     path('', include('home.urls')),
 ]
